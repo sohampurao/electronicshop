a916adb2a655674fb80bca894bc5b6f18484cbc1
Soham Purao <sohampurao1@gamil.com>
2022-12-04 23:50:14 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Electronic Shop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
