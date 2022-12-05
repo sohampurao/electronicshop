644b71f01440fc14820a4af8c958143a4a551f27
Soham Purao <sohampurao1@gamil.com>
2022-12-05 23:52:30 +0530

    products are fecthed form backend node server to frontend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect } from 'react';
+// import data from '../data';
 import '../css/home.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Homescreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <main>
         <h1 className="home-title">Featured Products</h1>
         <div className="products">
-          {data.products.map((product) => (
+          {products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
