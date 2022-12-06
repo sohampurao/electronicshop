adac09d0e19f52d818c7746051dfb642a54f9ae9
Soham Purao <sohampurao1@gamil.com>
2022-12-06 23:01:13 +0530

    react context store added to store cartitems and add to cart function added to the ATC buttons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import { Store } from '../Store';
 
 export default function Navigationbar() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <div>
       <header>
@@ -12,6 +18,16 @@ export default function Navigationbar() {
             <LinkContainer to="/">
               <Navbar.Brand>Gada Electronics</Navbar.Brand>
             </LinkContainer>
+            <Nav className="ms-auto">
+              <Link to="/cart" className="nav-link text-white">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <Badge pill bg="danger ms-1 fs-smaller">
+                    {cart.cartItems.length}
+                  </Badge>
+                )}
+              </Link>
+            </Nav>
           </Container>
         </Navbar>
       </header>
