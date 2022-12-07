b4c763ba4b2f605d542e7d4d3368477372734ae0
Soham Purao <sohampurao1@gamil.com>
2022-12-07 23:44:30 +0530

    created sign-in screen design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductScreen from './screens/ProductScreen';
 import Container from 'react-bootstrap/esm/Container';
 import Footer from './components/Footer';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
+            <Route path="/signin" element={<SigninScreen />} />
             <Route index element={<Homescreen />} />
           </Routes>
         </Container>
