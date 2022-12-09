287e95f476048394a9ad2ba18830eac8e4ffb02d
Soham Purao <sohampurao1@gamil.com>
2022-12-09 17:23:43 +0530

    products are seeded in mongoDB database and fetched to frontend from DB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import data from './data.js';
 import mongoose, { connect } from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
+mongoose.set('strictQuery', true);
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -14,28 +18,8 @@ mongoose
   });
 
 const app = express();
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 
