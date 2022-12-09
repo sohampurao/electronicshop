e15d80294f01b909c759bb38d8d722ed4c387973
Soham Purao <sohampurao1@gamil.com>
2022-12-09 19:01:30 +0530

    successfully implemented signin API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
-import mongoose, { connect } from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRouter.js';
 
 dotenv.config();
 mongoose.set('strictQuery', true);
@@ -18,11 +18,19 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
