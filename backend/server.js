f6803795866253790ddf5379231824a1d1b603d5
Soham Purao <sohampurao1@gamil.com>
2022-12-17 18:52:49 +0530

    deploy electronic shop

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRouter.js';
 import orderRouter from './routes/orderRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
+import path from 'path';
 
 dotenv.config();
 mongoose.set('strictQuery', true);
@@ -33,6 +34,13 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
