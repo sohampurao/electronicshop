c93434418498529009357da1ee19a790937ef7eb
Soham Purao <sohampurao1@gamil.com>
2022-12-30 07:27:40 +0530

    fix update

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,7 @@ app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
