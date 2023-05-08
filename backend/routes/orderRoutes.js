d27f4050b8fabb70b43ddee2962caa7df04c9cda
Soham Purao <94833514+sohampurao@users.noreply.github.com>
2023-05-08 13:24:33 +0530

    fix update

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import Order from '../models/orderModel.js';
+import Order from '../models/OrderModel.js';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
 import { isAuth, isAdmin } from '../utils.js';
